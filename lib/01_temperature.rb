d1947b5f942d8d1cf5fb223d3691611824526b18
jordan-creyelman <creyelmanjo@gmail.com>
2021-10-11 16:55:37 +0200

    first commit

---
+++ lib/01_temperature.rb
@@ -0,0 +1,9 @@
+def ftoc(temperature)
+     (temperature.to_f-32)*5/9
+end
+
+def ctof(temperature)
+      (temperature.to_f*9/5)+32
+end
+
+
---
+++ lib/05_timer.rb
@@ -0,0 +1,5 @@
+def time_string(sec)
+   return  "%02d:%02d:%02d" % [sec / 3600, sec / 60 % 60, sec % 60]
+  end
+
+  
